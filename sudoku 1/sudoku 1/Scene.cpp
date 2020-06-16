1bddcb438953485239c9c6b5358512f9fa8cca24
Sheng-Feng Lin <a8250157@g.pccu.edu.tw>
2020-06-16 20:07:46 +0800

    202006162007

---
+++ sudoku 1/sudoku 1/Scene.cpp
@@ -0,0 +1,21 @@
+//¼Æ¿WÀô¹Ò
+#include<iostream>
+#include <vector>
+#include"Scene.h"
+
+using namespace std;
+
+class Scene {
+public:
+	Scene(int);
+	
+	void Generater();
+	void show() const;
+	
+	bool setCurValue(const int, int&);
+	bool setPointValue();
+private:
+	int index;
+	int max_column;
+	
+};
